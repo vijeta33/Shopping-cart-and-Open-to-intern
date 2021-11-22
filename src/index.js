c61cb47c83f735832b66408445bd44c1cf72312c
Rahul271297 <rahul7004707854@gmail.com>
2021-11-22 09:46:59 +0530

    Assignment On MiddleWare

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 const express = require('express');
 var bodyParser = require('body-parser');
-
+const moment = require('moment')
 const route = require('./routes/route.js');
 
-const app = express();
+const app = express();  
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const logDataOnConsole = function(req,res,next){
+    let date = moment.utc().format('YYY-MM-DD HH:mm:ss');
+    let stillUtc = moment.utc(date).toDate();
+    let local = moment(stillUtc).local().format('YYY-MM-DD HH:mm:ss');
+    console.log(local);
+    console.log(req.ip);
+    console.log(req.url);
+    next();
+}
+app.use(logDataOnConsole)
 const mongoose = require('mongoose')
 
 mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/RahulKumar-DB?retryWrites=true&w=majority", {useNewUrlParser: true})
     .then(() => console.log('mongodb running on 27017'))
     .catch(err => console.log(err))
+
+    
+
+    
+
+    
 
 app.use('/', route);
 
